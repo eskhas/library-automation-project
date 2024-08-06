184b9eda5d5593988ed175f533b17c6d62bf0b98
eskhas <maliclay25@gmail.com>
2024-08-06 15:09:15 +0300

    member crud added and _layout is modified to show books at the home page ,Loans feature spoiler added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,33 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using library_automation.Models;
+using library_automation.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace library_automation.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    private readonly dbContext _context;
+    public HomeController(dbContext context, ILogger<HomeController> logger)
     {
+        _context = context;
         _logger = logger;
     }
-
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var books = from b in _context.Books
+                    select b;
+        return View(await books.ToListAsync());
     }
-
+    public async Task<IActionResult> Details(int? id)
+    {
+        var details = await _context.Books
+        .Include(a => a.Author)
+        .FirstOrDefaultAsync(x => x.Id == id);
+        return View(details);
+    }
     public IActionResult Privacy()
     {
         return View();
--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -1,48 +1,64 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using library_automation.Data;
 using library_automation.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 
 namespace library_automation.Controllers
 {
-    public class BookController : Controller
+    public class MemberController : Controller
     {
-        // GET: BookController
-        public async Task<IActionResult> Index()
-        {
-            var books = from b in _context.Books
-                        select b;
-            return View(await books.ToListAsync());
-        }
         private readonly dbContext _context;
-        public BookController(dbContext context)
+
+        public MemberController(dbContext context)
         {
             _context = context;
         }
-        // GET: Book/Create
+
+        // GET: Member
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Members.ToListAsync());
+        }
+
+        // GET: Member/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var member = await _context.Members
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            return View(member);
+        }
+
+        // GET: Member/Create
         public IActionResult Create()
         {
-            ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "FirstName");
             return View();
         }
 
-        // POST: Book/Create
+        // POST: Member/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,AuthorId,Title,Publisher,PublicationYear,Genre")] Book book)
+        public async Task<IActionResult> Create([Bind("Id,Name,Email,Phone,Address")] Member member)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(book);
+                _context.Add(member);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "FirstName", book.AuthorId);
-            return View(book);
+            return View(member);
         }
 
-        // GET: Book/Edit/5
+        // GET: Member/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -50,21 +66,20 @@ namespace library_automation.Controllers
                 return NotFound();
             }
 
-            var book = await _context.Books.FindAsync(id);
-            if (book == null)
+            var member = await _context.Members.FindAsync(id);
+            if (member == null)
             {
                 return NotFound();
             }
-            ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "FirstName", book.AuthorId);
-            return View(book);
+            return View(member);
         }
 
-        // POST: Book/Edit/5
+        // POST: Member/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,AuthorId,Title,Publisher,PublicationYear,Genre")] Book book)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Email,Phone,Address")] Member member)
         {
-            if (id != book.Id)
+            if (id != member.Id)
             {
                 return NotFound();
             }
@@ -73,12 +88,12 @@ namespace library_automation.Controllers
             {
                 try
                 {
-                    _context.Update(book);
+                    _context.Update(member);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!BookExists(book.Id))
+                    if (!MemberExists(member.Id))
                     {
                         return NotFound();
                     }
@@ -89,11 +104,10 @@ namespace library_automation.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["AuthorId"] = new SelectList(_context.Authors, "Id", "FirstName", book.AuthorId);
-            return View(book);
+            return View(member);
         }
 
-        // GET: Book/Delete/5
+        // GET: Member/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -101,35 +115,34 @@ namespace library_automation.Controllers
                 return NotFound();
             }
 
-            var book = await _context.Books
-                .Include(b => b.Author)
+            var member = await _context.Members
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (book == null)
+            if (member == null)
             {
                 return NotFound();
             }
 
-            return View(book);
+            return View(member);
         }
 
-        // POST: Book/Delete/5
+        // POST: Member/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var book = await _context.Books.FindAsync(id);
-            if (book == null)
+            var member = await _context.Members.FindAsync(id);
+            if (member == null)
             {
                 return NotFound();
             }
-            _context.Books.Remove(book);
+            _context.Members.Remove(member);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool BookExists(int id)
+        private bool MemberExists(int id)
         {
-            return _context.Books.Any(e => e.Id == id);
+            return _context.Members.Any(e => e.Id == id);
         }
     }
 }
